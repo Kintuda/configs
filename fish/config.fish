8e187ea1e709b361e0c41d1d45ece762d2717c1e
kin <kin@localhost.localdomain>
2019-06-29 15:37:06 -0300

    ADD: config

---
+++ fish/config.fish
@@ -0,0 +1,19 @@
+set -gx PATH $PATH:/home/$USER/Development/flutter/bin
+
+set --export ANDROID $HOME/Android;
+set --export ANDROID_HOME $ANDROID/Sdk;
+set -gx PATH ~/.npm-global/bin $PATH;
+set -gx PATH $ANDROID_HOME/tools $PATH;
+set -gx PATH $ANDROID_HOME/tools/bin $PATH;
+set -gx PATH $ANDROID_HOME/platform-tools $PATH;
+
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /home/kin/.npm-global/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /home/kin/.npm-global/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /home/kin/.npm-global/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /home/kin/.npm-global/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
+# tabtab source for slss package
+# uninstall by removing these lines or running `tabtab uninstall slss`
+[ -f /home/kin/.npm-global/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish ]; and . /home/kin/.npm-global/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish
